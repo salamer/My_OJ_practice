706a720b91b9840d5c70c16287c999c29cd21e28
aljun <salamer_gaga@163.com>
2016-06-03 15:20:04 +0800

    ~~~

--- c++/qsort.cpp
+++ c++/qsort.cpp
@@ -7,24 +7,53 @@ using namespace std;
 
 int a[NUM],n;
 
-int qsort(int start,int end){
-  q=partition(start,end);
-  qsort(start,q-1);
-  qsort(q+1,end);
+void swap(int i,int j){
+  int temp;
+  temp=a[i];
+  a[i]=a[j];
+  a[j]=temp;
 }
 
 int partition(int start,int end){
-  
+  int x=a[end];
+  int i=start-1;
+  for (int j=start;j<=end-1;j++){
+    if (a[j]<x){
+      i=i+1;
+      swap(i,j);
+    }
+  }
+  swap(i+1,end);
+  printf("part:%d\n",a[i+1]);
+  return i+1;
 }
+
+void qsort(int start,int end){
+  if (start<end){
+    for(int i=1;i<=n;i++){
+      printf("%d ",a[i]);
+    }
+    printf("\n");
+    int q=partition(start,end);
+
+    qsort(start,q-1);
+    qsort(q+1,end);
+  }
+}
+
 
 int main(){
 
   scanf("%d",&n);
-  for(int i=0;i<n;i++){
+  for(int i=1;i<=n;i++){
     scanf("%d",&a[i]);
-    printf("%d "a[i]);
+    printf("%d ",a[i]);
   }
   printf("\n");
-
-
+  qsort(1,n);
+  for(int i=1;i<=n;i++){
+    printf("%d ",a[i]);
+  }
+  printf("\n");
+  return 0;
 }
